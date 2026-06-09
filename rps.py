f7678238a84abfcf86438ea40970776109f6e735
mykasav <msavchykin@gmail.com>
2026-06-09 19:57:41 +0100

    Add files via upload
    Little rock paper scissors game that I made

---
+++ rps.py
@@ -0,0 +1,61 @@
+import random
+
+class RPS:
+
+    listrps=["r","p","s"]
+    def randomize(self):
+        listrpsrad=random.choice(self.listrps)
+        return listrpsrad
+    
+    def validateR(self,guess):
+        if guess=="r":
+            return "Stalemate!"
+        elif guess=="p":
+            return "You won!!"
+        elif guess=="s":
+            return "You lost!!"
+        
+    def validateP(self,guess):
+        if guess=="p":
+            return "Stalemate!"
+        elif guess=="r":
+            return "You lost"
+        elif guess=="s":
+            return "you won"
+        
+    def validateS(self,guess):
+        if guess=="s":
+            return "Stalemate!"
+        elif guess=="p":
+            return "You lost!!"
+        elif guess=="r":
+            return "You won"
+        
+    def start(self):
+            while True:
+                autorized=["r","s","p"]
+                guess=str(input("Chose Rock,Paper,Scissors or /leave: (r/p/s.)").lower())
+                if guess=="/leave":
+                    print("thx for playing")
+                    break
+                if guess not in autorized:
+                    print("Invalid input, try other beatch")
+                    continue
+                
+                a=self.randomize()
+                print("o robo escolheu",a)
+                if a=="r":
+                    print( self.validateR(guess))
+                        
+                elif a=="p":
+                    print( self.validateP(guess))
+                    
+                elif a=="s":
+                    print( self.validateS(guess))
+           
+                
+            
+gamerps=RPS()
+resultado=gamerps.start()
+print(resultado)
+            
